a6b20e7d42a0867733a9f7a72ccaf823f1f17f03
ergoxeiras-repo <gouc53@gmail.com>
2021-04-08 16:01:35 +0300

    static_root was added

--- ergoxeiras/settings.py
+++ ergoxeiras/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 MEDIA_URL = '/images/'
