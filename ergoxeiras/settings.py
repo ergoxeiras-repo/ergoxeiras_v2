b79f714c29acda2bc3938767a0d06ded8f00adb3
ergoxeiras-repo <gouc53@gmail.com>
2021-04-08 16:11:34 +0300

    allowed hosts was updated

--- ergoxeiras/settings.py
+++ ergoxeiras/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8*pcs1p4!9u_tsa+m0#cc4z*zuv$(6uu$gx8jklm+iu+vfb2t=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ergoxeirasapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
