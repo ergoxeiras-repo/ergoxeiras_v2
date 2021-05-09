331b2cdfd84e4141dac506b10d9cd852c9751d8e
ergoxeiras-repo <gouc53@gmail.com>
2021-05-09 13:20:03 +0300

    admin page was updated

--- pages/admin.py
+++ pages/admin.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import Group
 from .models import Image
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'uploaded_at')
-    list_filter = ('uploaded_at',)
+    list_display = ('title', 'uploaded_at', 'category')
+    list_filter = ('uploaded_at', 'category')
 
 admin.site.site_header = 'Εργοχειράς Σελίδα Διαχείρησης'
 admin.site.register(Image, ImageAdmin)
